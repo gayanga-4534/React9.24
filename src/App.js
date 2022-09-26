6d76df5566e1df56a0b935b5d5ee2fe71c3bd39d
gayanga-4534 <gayanhasandaruwan@gmail.com>
2022-09-26 15:19:57 +0530

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ToastContainer} from 'react-toastify';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import AddContact from './components/AddContact';
+import EditContact from './components/EditContact';
+const App=()=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={()=><Home/>}>
+
+</Route>
+
+<Route path="/add">
+<AddContact/> 
+</Route>
+
+<Route path="/edit/:id">
+<EditContact/>
+</Route>
+</Switch>
     </div>
   );
 }
---
+++ src/components/AddContact.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+
+const AddContact = () => {
+    return (
+        <div className="container">
+                   <h1 className="display-3 my-5 text-center">
+          Add Student
+        </h1>
+             <div className="row">
+            
+  
+
+
+        <div className="col-md-6 shadow mx-auto p-5">
+       <form>
+       <div className="form-group">
+       <input type="text" placeholder="Name" className="form-control"/>
+            </div>
+
+            <div className="form-group">
+       <input type="email" placeholder="Email" className="form-control"/>
+            </div>
+
+            <div className="form-group">
+       <input type="number" placeholder="Phone number" className="form-control"/>
+            </div>
+
+            <div className="form-group">
+       <input type="submit" value="Add Student" className="btn btn-block btn-dark"/>
+            </div>
+
+       </form>
+            </div>
+
+            </div>
+        </div> 
+    )
+}
+
+
+
+export default AddContact
